9ed567ed3e38ca3f6c9cdc59220a38ff41b7e2c0
amir <arziai@gmail.com>
2023-11-12 16:20:14 -0800

    plot

--- rafic/experiments.py
+++ rafic/experiments.py
@@ -1,6 +1,7 @@
 import typing as t
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from . import config
 from .birds_data import get_birds_dataloader
@@ -13,7 +14,7 @@ AS = [0, 1, 2, 5]
 
 def _get_val_dataloader(k, a):
     return get_birds_dataloader(
-        split='val',
+        split="val",
         batch_size=16,
         num_way=5,
         num_support=k,
@@ -24,9 +25,10 @@ def _get_val_dataloader(k, a):
         num_aug=a,
     )
 
+
 def zero_shot_text_label():
     dl = get_birds_dataloader(
-        split='val',
+        split="val",
         batch_size=16,
         num_way=5,
         num_support=1,
@@ -43,10 +45,13 @@ def zero_shot_text_label():
 def non_parametric_image_embeddings(k_vals: KS, a_vals: AS) -> pd.DataFrame:
     def _fn(k, a):
         return Evaluation.eval_non_parametric_nn(dl=_get_val_dataloader(k=k, a=a))
+
     return _run(k_vals=k_vals, a_vals=a_vals, acc_fn=_fn)
 
 
-def logistic_regression(k_vals: KS, a_vals: AS, seed: int = config.SEED) -> pd.DataFrame:
+def logistic_regression(
+    k_vals: KS, a_vals: AS, seed: int = config.SEED
+) -> pd.DataFrame:
     def _fn(k, a):
         return Evaluation.eval_clf(dl=_get_val_dataloader(k=k, a=a), seed=seed)
 
@@ -55,11 +60,14 @@ def logistic_regression(k_vals: KS, a_vals: AS, seed: int = config.SEED) -> pd.D
 
 def _run(k_vals: KS, a_vals: AS, acc_fn: t.Callable) -> pd.DataFrame:
     return pd.DataFrame(
-        dict(
-            num_support=k,
-            num_aug=a,
-            acc=acc_fn(k=k, a=a)
-        )
+        dict(num_support=k, num_aug=a, acc=acc_fn(k=k, a=a))
         for k in k_vals
         for a in a_vals
     )
+
+
+def plot_val_accuracy_vs_support(df: pd.DataFrame):
+    df.pivot_table(index="num_support", columns="num_aug", values="acc").plot()
+    plt.ylabel("Validation accuracy")
+    title = "Validation accuracy vs. number of support images with different number of augmented images"
+    _ = plt.title(title)
