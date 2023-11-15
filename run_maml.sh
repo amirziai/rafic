0da0dee403e51cd2a71ed61caab042e1aa7a09b5
limiao2 <miaolior@gmail.com>
2023-11-15 06:03:53 +0000

    update log interval and command

--- run_maml.sh
+++ run_maml.sh
@@ -1,1 +1,5 @@
-python3 -m rafic.maml --inner_lr 0.04 --num_inner_steps 5 --batch_size 4 --device gpu --num_way 5 --num_support 3 --num_query 10 --num_aug 0
+# support 1, augment 0, 1, 2, 5
+python3 -m rafic.maml --inner_lr 0.04 --num_train_iterations 1000 --num_inner_steps 5 --batch_size 64 --device gpu --num_way 5 --num_support 1 --num_aug 0
+python3 -m rafic.maml --inner_lr 0.04 --num_train_iterations 1000 --num_inner_steps 5 --batch_size 64 --device gpu --num_way 5 --num_support 1 --num_aug 1
+python3 -m rafic.maml --inner_lr 0.04 --num_train_iterations 1000 --num_inner_steps 5 --batch_size 64 --device gpu --num_way 5 --num_support 1 --num_aug 2
+python3 -m rafic.maml --inner_lr 0.04 --num_train_iterations 1000 --num_inner_steps 5 --batch_size 64 --device gpu --num_way 5 --num_support 1 --num_aug 5
