cf59fbe3f18b0999016357a3e83900bf5f7f466f
Hangfei Lin <hangfeilin@gmail.com>
2023-11-29 21:14:45 -0800

    Update config.py

--- rafic/config.py
+++ rafic/config.py
@@ -2,6 +2,7 @@ _PATH_BASE_LAION = "/home/cs330-user-lm/cs330_hw/data/laion"
 BASE_PATH = "/home/cs330-user-lm/cs330_hw/data/birds/CUB_200_2011"
 # image folders(each class in its own folder) should be under images subdirectory. embeddings should be under embeddings subdirectory.
 EURO_SAT_BASE_PATH = "/home/cs330-user-lm/cs330_hw/data/EuroSAT_RGB"
+FOOD_101_BASE_PATH = "//home/cs330-user-lm/cs330_hw/data/food101/"
 PATH_SEARCH = f"{_PATH_BASE_LAION}/faiss/clip-laion-400m-1m.pkl"
 PATH_SEARCH_EMB = _PATH_BASE_LAION
 PATH_FAISS_INDEX = f"{_PATH_BASE_LAION}/faiss/clip-laion-400m-1m-faiss.index"
